9205ceebbad871005682f362bfc9198bc2095398
Robin van der Noord <robinvandernoord@gmail.com>
2024-04-12 17:55:30 +0200

    docs: added more info to `plugin.add`

--- src/edwh/local_tasks/plugin.py
+++ src/edwh/local_tasks/plugin.py
@@ -241,11 +241,13 @@ def remove_all(c):
 @task(aliases=("install",))
 def add(c, plugin_names: str):
     """
-    Install a new plugin
+    Install a new plugin.
 
     Args:
         c (Context): invoke ctx
-        plugin_name: which plugin to add
+        plugin_names: which plugin(s) to add. You can add multiple plugins by separating them with a comma
+            (e.g. `edwh plugin.add restic,multipass,bundler`).
+            You can install all plugins by using 'all': `edwh plugin.add all`.
     """
     if plugin_names == "all":
         return add_all(c)
